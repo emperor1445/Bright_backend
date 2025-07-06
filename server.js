c4e2d312acb7646781e0aef6e268e304d7b70eab
uzigirl <uzumakitango@gmail.com>
2025-07-07 00:44:02 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const transactionRoutes = require("./routes/transactions");
 
