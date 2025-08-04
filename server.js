664cf025f23e46f17c9e3fe5d4570e996a77030f
uzigirl <uzumakitango@gmail.com>
2025-08-04 02:56:26 +0100

    modified:   server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const transactionRoutes = require("./routes/transactions");
 
 const app = express();
 const corsOptions = {
-  origin: "https://bright-project-main.vercel.app", // or "*" temporarily to test
+  origin: "https://bright-project-main-wz5c.vercel.app/", // or "*" temporarily to test
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -17,7 +17,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 
 // Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://davidemperor1445:prospermongodb@cluster0.igwin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
 
 .then(() => {
   console.log("✅ MongoDB connected");
@@ -28,36 +28,3 @@ mongoose.connect("mongodb+srv://davidemperor1445:prospermongodb@cluster0.igwin.m
 .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 
-
-
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-
-// const authRoutes = require("./routes/auth");
-// const transactionRoutes = require("./routes/transactions");
-
-// const app = express();
-
-// // ✅ HERE: CORS config should be added before any routes or body parsing
-// app.use(cors({
-//   origin: "https://bright-project-main.vercel.app/"  // <-- replace with your actual frontend URL
-// }));
-
-// app.use(bodyParser.json());
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/transactions", transactionRoutes);
-
-// // Connect to MongoDB Atlas
-// mongoose.connect(
-//   "mongodb+srv://davidemperor1445:prospermongodb@cluster0.igwin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-// )
-// .then(() => {
-//   console.log("✅ MongoDB connected");
-
-//   const PORT = 5000;
-//   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-// })
-// .catch((err) => console.error("❌ MongoDB connection error:", err));
